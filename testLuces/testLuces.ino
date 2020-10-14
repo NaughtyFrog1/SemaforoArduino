9af5f12c5e4983c84520026c45b923f604745b6f
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-10-14 10:46:07 -0300

    testLuces implementa POO

--- testLuces/testLuces.ino
+++ testLuces/testLuces.ino
@@ -1,28 +1,61 @@
 #include <Arduino.h>
 
-typedef const byte pin;
-pin ROJO = 12;
-pin AMBAR = 11;
-pin VERDE = 10;
 
-void setup() {
-  pinMode(ROJO, OUTPUT);
-  pinMode(AMBAR, OUTPUT);
-  pinMode(VERDE, OUTPUT);
+class testLuces
+{
+private:
+  byte pinLuz;
+  
+public:
+  testLuces();
+  void begin(byte pin);
+  void on();
+  void off();
+};
 
+testLuces::testLuces(/* args */)
+{
+  pinLuz = 0;
 }
 
-void loop() {
-  digitalWrite(ROJO, HIGH);
-  delay(2000);
-  digitalWrite(ROJO, LOW);
+void testLuces::begin(byte pin)
+{
+  pinLuz = pin;
+  pinMode(pinLuz, OUTPUT);
+}
 
-  digitalWrite(VERDE, HIGH);
-  delay(2000);
-  digitalWrite(VERDE, LOW);
+void testLuces::on() {
+  digitalWrite(pinLuz, HIGH);
+}
 
-  
-  digitalWrite(AMBAR, HIGH);
+void testLuces::off() {
+  digitalWrite(pinLuz, LOW);
+}
+
+
+testLuces led1, led2, led3;
+
+void setup()
+{
+  led1.begin(12);
+  led2.begin(11);
+  led3.begin(10);
+}
+
+void loop()
+{
+  led1.on();
+  delay(2000);
+  led2.on();
   delay(1000);
-  digitalWrite(AMBAR, LOW);
+  led1.off();
+  led2.off();
+
+  led3.on();
+  delay(2000);
+  led3.off();
+
+  led2.on();
+  delay(1000);
+  led2.off();
 }
