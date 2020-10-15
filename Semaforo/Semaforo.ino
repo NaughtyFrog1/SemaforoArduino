91775fb7fc76479d653de6889e146cd1dc98dee1
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-10-14 23:06:26 -0300

    Inicializadas los 4 semaforos y la esquina

--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+// #include <iostream>
 
 
 
@@ -138,7 +139,7 @@ class TestConexiones
 {
   private:
     Timer t;
-    Semaforo sem1, sem2, sem3, sem4;
+    Semaforo s1, s2, s3, s4;
   public:
     void begin(byte r1, byte a1, byte v1, 
                byte r2, byte a2, byte v2,
@@ -152,17 +153,17 @@ void TestConexiones::begin(byte r1, byte a1, byte v1,
                            byte r3, byte a3, byte v3,
                            byte r4, byte a4, byte v4)
 {
-  sem1.begin(r1, a1, v1);
-  sem2.begin(r2, a2, v2);
-  sem3.begin(r3, a3, v3);
-  sem4.begin(r4, a4, v4);
+  s1.begin(r1, a1, v1);
+  s2.begin(r2, a2, v2);
+  s3.begin(r3, a3, v3);
+  s4.begin(r4, a4, v4);
 }
 
 void TestConexiones::testAll()
 {
   for (int i = 0; i < 4; i++)
   {
-    Semaforo semaforo[4] = {sem1, sem2, sem3, sem4};
+    Semaforo semaforo[4] = {s1, s2, s3, s4};
 
     semaforo[i].allOn();
     t.Stop(0.5);
@@ -224,6 +225,9 @@ void TestEstados::testRA()
 
 Timer tm;
 
+Semaforo sem1, sem2, sem3, sem4;
+
+
 void setup()
 {
   //· Tests ------------------------------------------------------------------>
@@ -245,9 +249,16 @@ void setup()
   est.endTest();
 
   //· Programa --------------------------------------------------------------->
-
+  
+  sem1.begin(14, 15, 16);
+  sem2.begin(17, 18, 19);
+  sem3.begin(12, 11, 10);
+  sem4.begin(9, 8, 7);
 }
 
 void loop()
 {
+  Semaforo esquina[4] = {sem1, sem2, sem3, sem4};
+
+  esquina[1].allOn();
 }
