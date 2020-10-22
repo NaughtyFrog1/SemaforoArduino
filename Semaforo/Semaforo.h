594ff09b111f21ccb7b6f112af65c800144b7123
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-10-22 20:31:55 -0300

    rm time para luz roja

--- Semaforo/Semaforo.h
+++ Semaforo/Semaforo.h
@@ -12,12 +12,21 @@ class LuzSemaforo
     bool is_on;
     unit_t time_on;
   public:
-    void begin(byte pin, unit_t t);
+    void begin(byte pin_luz);  // Para luz roja
+    void begin(byte pin_luz, unit_t t);
     void on();
     void off();
     bool getSt();
     unit_t getTimeOn();
 };
+
+void LuzSemaforo::begin(byte pin_luz)
+{
+  pin = pin_luz;
+  is_on = false;
+  time_on = 0;
+  pinMode(pin, OUTPUT);
+}
 
 void LuzSemaforo::begin(byte pin_luz, unit_t t)
 {
@@ -39,10 +48,9 @@ void LuzSemaforo::off()
   is_on = false;
 }
 
-bool LuzSemaforo::getSt()
-{
-  return is_on;
-}
+bool LuzSemaforo::getSt() {return is_on;}
+
+unit_t LuzSemaforo::getTimeOn() {return time_on;}
 
 
 //· Semaforo ----------------------------------------------------------------->
@@ -53,7 +61,7 @@ class Semaforo
     LuzSemaforo rojo, ambar, verde;
     unit_t last_change;
   public:
-    void begin(byte r, byte a, byte v, unit_t tr, unit_t ta, unit_t tv);
+    void begin(byte r, byte a, byte v, unit_t tv, unit_t ta);
     void allOn();
     void allOff();
     void setR();
@@ -63,16 +71,15 @@ class Semaforo
     bool getStR();
     bool getStA();
     bool getStV();
-    unit_t getTimeOnR();
-    unit_t getTimeOnA();
     unit_t getTimeOnV();
+    unit_t getTimeOnA();
     unit_t getLastChange();
 
 };
 
-void Semaforo::begin(byte r, byte a, byte v, unit_t tr, unit_t ta, unit_t tv)
+void Semaforo::begin(byte r, byte a, byte v, unit_t tv, unit_t ta)
 {
-  rojo.begin(r, tr);
+  rojo.begin(r);
   ambar.begin(a, ta);
   verde.begin(v, tv);
 }
@@ -117,13 +124,13 @@ void Semaforo::setRA()
   allOff();
   rojo.on();
   ambar.on();
+  last_change = millis();
 }
 
 bool Semaforo::getStR() {return rojo.getSt();}
 bool Semaforo::getStA() {return ambar.getSt();}
 bool Semaforo::getStV() {return verde.getSt();}
 
-unit_t Semaforo::getTimeOnR() {return rojo.getTimeOn();}
 unit_t Semaforo::getTimeOnA() {return ambar.getTimeOn();}
 unit_t Semaforo::getTimeOnV() {return verde.getTimeOn();}
 
