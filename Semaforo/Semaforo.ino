83be5e6a942bbb4751b7b7292afc9d99fe613f6d
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-11-13 19:49:03 -0300

    Optimizar cant arrays para lista_sem

--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -56,21 +56,20 @@ EsquinaSimple esq1(2), esq2(2);
 
 void setup() { 
   TestConexiones conex( 14, 15, 16, 17, 18, 19, 12, 11, 10, 9, 8, 7);
+  Semaforo lista_sem[2];
 
-  Semaforo lista_sem_esq1[2];
-  lista_sem_esq1[0] = Semaforo(14, 15, 16, 3000, 1000);
-  lista_sem_esq1[1] = Semaforo(17, 18, 19, 3000, 1000);
 
-  esq1.setSemaforos(lista_sem_esq1);
+  lista_sem[0] = Semaforo(14, 15, 16, 3000, 1000);
+  lista_sem[1] = Semaforo(17, 18, 19, 3000, 1000);
+  esq1.setSemaforos(lista_sem);
 
-  Semaforo lista_sem_esq2[2];
-  lista_sem_esq2[0] = Semaforo(12, 11, 10, 4000, 1000);
-  lista_sem_esq2[1] = Semaforo( 9, 8, 7, 4000, 1000);
+  lista_sem[0] = Semaforo(12, 11, 10, 4000, 1000);
+  lista_sem[1] = Semaforo( 9, 8, 7, 4000, 1000);
+  esq2.setSemaforos(lista_sem);
 
-  esq1.setSemaforos(lista_sem_esq1);
-  esq2.setSemaforos(lista_sem_esq2);
 
   conex.testAll();
+
   esq1.todasRojo();
   esq2.todasRojo();
 }
