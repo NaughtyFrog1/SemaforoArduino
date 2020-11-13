cb685e0acea9cc7da675ce4421877fcd857f09eb
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-11-13 17:01:44 -0300

    Esquina cantidad semaforos indefinida funciona

--- Semaforo/Semaforo.h
+++ Semaforo/Semaforo.h
@@ -68,6 +68,7 @@ class MuchiEsquinas : public Esquina {
   private:
     Semaforo sem[8];
     byte cant_sem;
+    byte curr_sem;
     byte last_sem;
     byte max_step;
   
@@ -202,6 +203,7 @@ MuchiEsquinas::MuchiEsquinas(byte c) {
   max_step = (cant_sem * 2) - 1;
   step = 0;
   last_step = 0;
+  curr_sem = 0;
   last_sem = cant_sem - 1;
 }
 
@@ -216,9 +218,36 @@ void MuchiEsquinas::todasRojo() {
 }
 
 void MuchiEsquinas::secuencia() {
-  sem[0].setV(); 
-  sem[1].setA();
-  sem[2].setRA();
+  for (byte stp = 0; stp <= max_step; stp += 2) {
+    if ( 
+      (step == stp)
+      && (millis() > last_step + sem[last_sem].getTimeOnA())
+    ){
+      sem[curr_sem].setV();
+      sem[last_sem].setR();
+
+      step++;
+      last_step = millis();
+    }
+
+    else if (
+      (step == stp + 1)
+      && (millis() > last_step + sem[curr_sem].getTimeOnV())
+    ){
+      sem[curr_sem].setA();
+      sem[(curr_sem + 1) % cant_sem].setRA();
+
+      step++;
+      last_step = millis();
+      last_sem = curr_sem;
+      curr_sem++;
+    }
+  }
+  if (step > max_step) {
+    step = 0;
+    last_sem = cant_sem - 1;
+    curr_sem = 0;
+  }
 }
 
 
