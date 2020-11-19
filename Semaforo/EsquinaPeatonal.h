5b9352a8d0a0168b75211a084eb0d398e97b9989
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-11-18 23:56:15 -0300

    EsquinaPeatonal Funciona

--- Semaforo/EsquinaPeatonal.h
+++ Semaforo/EsquinaPeatonal.h
@@ -18,9 +18,14 @@
 
 
 class SemaforoPeatonal {
+  private:
+    Luz no_cruce, cruce;
   public:
     SemaforoPeatonal();
-    void begin();
+    SemaforoPeatonal(byte nc, byte c);
+    void begin(byte nc, byte c);
+    void allOn();
+    void allOff();
     void setCruce();
     void setNoCruce();
 }; 
@@ -30,9 +35,7 @@ class EsquinaPeatonal : public Esquina {
     SemaforoPeatonal semP[8];
   public:
     EsquinaPeatonal(byte c);
-    void setSemaforos(Semaforo s[]);
-    void setPeatonales(Luz l[]);
-    
+    void setSemaforos(Semaforo s[], SemaforoPeatonal p[]);
     void secuencia();
     void todasRojo();
 };
@@ -42,7 +45,105 @@ class EsquinaPeatonal : public Esquina {
 // DECLARACIÓN DE LOS MÉTODOS ------------------------------------------------>
 
 
-// Code...
+//· SemaforoPeatonal
+
+SemaforoPeatonal::SemaforoPeatonal() { }
+
+SemaforoPeatonal::SemaforoPeatonal(byte nc, byte c) {
+  begin(nc, c);
+}
+
+void SemaforoPeatonal::begin(byte nc, byte c) {
+  no_cruce.begin(nc);
+  cruce.begin(c);
+}
+
+void SemaforoPeatonal::allOn() {
+  no_cruce.on();
+  cruce.on();
+}
+
+void SemaforoPeatonal::allOff() {
+  no_cruce.off();
+  cruce.off();
+}
+
+void SemaforoPeatonal::setNoCruce() {
+  no_cruce.on();
+  cruce.off();
+}
+
+void SemaforoPeatonal::setCruce() {
+  no_cruce.off();
+  cruce.on();
+}
+
+
+//· EsquinaPeatonal
+
+EsquinaPeatonal::EsquinaPeatonal(byte c) {
+  cant_sem = c;
+  max_step = (cant_sem * 2) - 1;
+  last_sem = cant_sem - 1;
+
+  step = 0;
+  last_step = 0;
+  curr_sem = 0;
+}
+
+void EsquinaPeatonal::setSemaforos(Semaforo s[], SemaforoPeatonal p[]) {
+  for (byte i = 0; i < cant_sem; i++) {
+    sem[i] = s[i];
+    semP[i] = p[i];
+  }
+}
+
+void EsquinaPeatonal::todasRojo() {
+  for (byte i = 0; i < cant_sem; i++) {
+    sem[i].setR();
+    semP[i].setCruce();
+  }
+  
+}
+
+void EsquinaPeatonal::secuencia() {
+  for (byte stp = 0; stp <= max_step; stp += 2) {
+    if ( 
+      (step == stp)
+      && (millis() > last_step + sem[last_sem].getTimeOnA())
+    ){
+      sem[curr_sem].setV();
+      sem[last_sem].setR();
+
+      semP[curr_sem].setNoCruce();
+      semP[last_sem].setCruce();
+
+      step++;
+      last_step = millis();
+    }
+
+    else if (
+      (step == stp + 1)
+      && (millis() > last_step + sem[curr_sem].getTimeOnV())
+    ){
+      sem[curr_sem].setA();
+      sem[(curr_sem + 1) % cant_sem].setRA();
+
+      semP[(curr_sem + 1) % cant_sem].setNoCruce();
+
+
+      last_sem = curr_sem;
+      curr_sem++;
+      step++;
+      last_step = millis();
+    }
+  }
+  if (step > max_step) {
+    step = 0;
+    last_sem = cant_sem - 1;
+    curr_sem = 0;
+  }
+}
 
 
 #endif
--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -56,7 +56,7 @@ void loop() {esq.secuencia();}
 /**/
 
 
-//* EsquinaSimple2x2 --------------------------------------------------------->
+/* EsquinaSimple2x2 --------------------------------------------------------->
 
 EsquinaSimple esq1(2), esq2(2);
 
@@ -89,4 +89,35 @@ void loop() {
   esq1.secuencia();
   esq2.secuencia();
 }
+/**/
+
+
+//* EsquinaPeatonal2 --------------------------------------------------------->
+EsquinaPeatonal esq(2);
+
+void setup() {
+  Semaforo conexiones[4];
+  conexiones[0] = Semaforo(14, 15, 16, 3000, 1000); 
+  conexiones[1] = Semaforo(17, 18, 19, 3000, 1000);
+  conexiones[2] = Semaforo(12, 11, 10, 4000, 1000);
+  conexiones[3] = Semaforo( 9, 8, 7, 4000, 1000);
+
+  TestConexiones conex(4, conexiones);
+
+  Semaforo lista_sem[2];
+  SemaforoPeatonal lista_semP[2];
+
+  lista_sem[0] = Semaforo(14, 15, 16, 3000, 1000);
+  lista_sem[1] = Semaforo(12, 11, 10, 4000, 1000);
+  
+  lista_semP[0] = SemaforoPeatonal(17, 18);
+  lista_semP[1] = SemaforoPeatonal(9, 8);
+
+  esq.setSemaforos(lista_sem, lista_semP);
+
+  conex.testAll();
+  esq.todasRojo();  
+}
+
+void loop() { esq.secuencia(); }
 /**/
