5ce70341fdb12401c064006750c147b0fcfe9422
Manuel Silenzi <manuelsilenzi1@gmail.com>
2020-11-19 11:15:05 -0300

    add: Comentarios a SemaforoPeatonal.h

--- Semaforo/EsquinaPeatonal.h
+++ Semaforo/EsquinaPeatonal.h
@@ -19,7 +19,7 @@
 
 class SemaforoPeatonal {
   private:
-    Luz no_cruce, cruce;
+    Luz no_cruce, cruce;  // Luces que conforman el semaforo
   public:
     SemaforoPeatonal();
     SemaforoPeatonal(byte nc, byte c);
@@ -32,7 +32,9 @@ class SemaforoPeatonal {
 
 class EsquinaPeatonal : public Esquina {
   private:
+    // Semáforos peatonales que debe controlar la esquina
     SemaforoPeatonal semP[8];
+    // Flag que indica cual es el siguiente semáforo en la secuencia
     byte next_sem;
   public:
     EsquinaPeatonal(byte c);
@@ -51,41 +53,67 @@ class EsquinaPeatonal : public Esquina {
 
 //· SemaforoPeatonal
 
-SemaforoPeatonal::SemaforoPeatonal() { }
+SemaforoPeatonal::SemaforoPeatonal() {
+  // Constructor vacio
+  // Útil cuando se desea inicializar con el método .begin()
+}
 
 SemaforoPeatonal::SemaforoPeatonal(byte nc, byte c) {
+  // Constructor que permite inicializar las luces del semáforo peatonal,
+  // indicanto a que pines deben ir conectadas.
+  // Útil para pasar objetos temporales
+
   begin(nc, c);
 }
 
 void SemaforoPeatonal::begin(byte nc, byte c) {
+  // Inicializar las luces del semáforo, indicando a que pines deben ir 
+  // conectadas
+
   no_cruce.begin(nc);
   cruce.begin(c);
 }
 
 void SemaforoPeatonal::allOn() {
+  // Encender todas las luces del semáforo peatonal
+
   no_cruce.on();
   cruce.on();
 }
 
 void SemaforoPeatonal::allOff() {
+  // Apagar todas las luces del semáforo peatonal
+
   no_cruce.off();
   cruce.off();
 }
 
 void SemaforoPeatonal::setNoCruce() {
+ // Poner el semáforo peatonal en `No Cruce'
+
   no_cruce.on();
   cruce.off();
 }
 
 void SemaforoPeatonal::setCruce() {
+  // Poner el semáforo peatonal en 'Cruce'
+
   no_cruce.off();
   cruce.on();
 }
 
 
 //· EsquinaPeatonal
+/*
+  Esta clase funciona de forma muy similar a la clase EsquinaSimple, por lo que
+  la mayoría de los métodos hacen lo mismo solo que le añaden la funcionalidad
+  de los semáforos peatonales
+*/
 
 EsquinaPeatonal::EsquinaPeatonal(byte c) {
+  // Determinar la cantidad de semaforos que habrá en la esquina y en base a
+  // eso dar valor a las propiedades del objeto
+
   cant_sem = c;
   max_step = (cant_sem * 2) - 1;
   last_sem = cant_sem - 1;
@@ -97,6 +125,11 @@ EsquinaPeatonal::EsquinaPeatonal(byte c) {
 }
 
 void EsquinaPeatonal::setSemaforos(Semaforo s[], SemaforoPeatonal p[]) {
+  // Recibe como argumento dos arrays, en uno se debe indicar que semáforos
+  // debe utilizar para la secuencia, en otro los semáforos peatonales.
+  // Es importante que el semáforo este en el mismo índice que el semáforo
+  // peatonal que le corresponde para que el programa funcione adecuadamente
+
   for (byte i = 0; i < cant_sem; i++) {
     sem[i] = s[i];
     semP[i] = p[i];
@@ -106,6 +139,10 @@ void EsquinaPeatonal::setSemaforos(Semaforo s[], SemaforoPeatonal p[]) {
 void EsquinaPeatonal::setSemaforos(
   Semaforo s[], byte pos_s,  SemaforoPeatonal p[], byte pos_p
 ){
+  // Recibe cuatro argumentos de entrada. Se indica el array de semáforos y la
+  // posición desde la que debe empezar y el array de los semáforos peatonales
+  // y el índice desde el que debe empezar
+
   byte offset_s = pos_s + cant_sem;
   byte offset_p = pos_p + cant_sem;
   byte i = 0;
@@ -123,6 +160,8 @@ void EsquinaPeatonal::setSemaforos(
 }
 
 void EsquinaPeatonal::init() {
+  // Poner en rojo todos los semáfotos y cruces peatonales
+
   for (byte i = 0; i < cant_sem; i++) {
     sem[i].setR();
     semP[i].setNoCruce();
@@ -131,6 +170,9 @@ void EsquinaPeatonal::init() {
 }
 
 void EsquinaPeatonal::secuencia() {
+  // Similar al método secuencia de la clase EsquinaSimple, pero se agregan los
+  // semáforos peatonales a la secuencia
+  
   for (byte stp = 0; stp <= max_step; stp += 2) {
     if ( 
       (step == stp)
